42169e37faaae7af25574f01040970c3fdacdf0f
levu2207 <levu2207@gmail.com>
2022-08-15 00:10:39 +0700

    use Yup validation

--- js/utils/post-form.js
+++ js/utils/post-form.js
@@ -1,4 +1,5 @@
-import { setBackgroundImage, setFieldValue } from './common';
+import { setBackgroundImage, setFieldValue, setTextContent } from './common';
+import * as yup from 'yup';
 
 function setFormValues(form, formValues) {
   setFieldValue(form, '[name="title"]', formValues?.title);
@@ -10,9 +11,99 @@ function setFormValues(form, formValues) {
   setBackgroundImage(document, '#postHeroImage', formValues?.imageUrl);
 }
 
+function getformValues(form) {
+  const formValues = {};
+
+  // S1: query each input and add to values object
+  // ['title', 'author', 'description', 'imageUrl'].forEach((name) => {
+  //   const field = form.querySelector(`[name="${name}"]`);
+  //   if (field) formValues[name] = field.value;
+  // });
+
+  // S2: using form data
+  const data = new FormData(form);
+
+  for (const [key, value] of data) {
+    formValues[key] = value;
+  }
+
+  return formValues;
+}
+
+function getPostSchema() {
+  return yup.object().shape({
+    title: yup.string().required('Please enter title'),
+    author: yup
+      .string()
+      .required('Please enter author')
+      .test(
+        'at-least-two-word',
+        'Please enter at least two words',
+        (value) => value.split(' ').filter((x) => !!x && x.length >= 3).length >= 2
+      ),
+    description: yup.string(),
+  });
+}
+
+function setFieldError(form, name, error) {
+  const element = form.querySelector(`[name="${name}"]`);
+  if (element) {
+    element.setCustomValidity(error);
+    setTextContent(element.parentElement, '.invalid-feedback', error);
+  }
+}
+
+async function validatePostForm(form, formValues) {
+  try {
+    // reset previous errors
+    ['title', 'author'].forEach((name) => setFieldError(form, name, ''));
+
+    // start validating
+    const schema = getPostSchema();
+    await schema.validate(formValues, { abortEarly: false });
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.inner);
+
+    const errorLog = {};
+
+    if (error.name === 'ValidationError' && Array.isArray(error.inner)) {
+      for (const validationError of error.inner) {
+        const name = validationError.path;
+
+        //  ignore if the field is alreday logged
+        if (errorLog[name]) continue;
+
+        // set field error and mark as logged
+        setFieldError(form, name, validationError.message);
+        errorLog[name] = true;
+      }
+    }
+  }
+
+  // add was-validated class to form element
+  const isValid = form.checkValidity();
+  if (!isValid) form.classList.add('was-validated');
+
+  return isValid;
+}
+
 export function initPostForm({ formId, defaultValues, onSubmit }) {
   const form = document.getElementById(formId);
   if (!form) return;
 
   setFormValues(form, defaultValues);
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // get form value
+    const formValues = getformValues(form);
+    console.log('form values', formValues);
+
+    // validation
+    // if valid trigger submit callback
+    // otherwise, show validation errors
+    if (!validatePostForm(form, formValues)) return;
+  });
 }
